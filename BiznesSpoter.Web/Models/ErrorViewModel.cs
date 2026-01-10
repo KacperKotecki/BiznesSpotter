f4e68ebe15df4b11c4af480126ef9709413343f8
Kacper Kotecki <kacperkotecki@o2.pl>
2026-01-10 18:30:12 +0100

    Wstępna konfiguracja projektu .gitignore .dockerignore Dockerfile

---
+++ BiznesSpoter.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BiznesSpoter.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
